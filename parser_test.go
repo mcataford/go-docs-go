f37816b5b56edaacd6d0a93817e5ec05b6eebc53
Marc Cataford <mcat@riseup.net>
2023-04-20 00:04:57 -0400

    test: coverage, leading comments on classes

--- parser_test.go
+++ parser_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"golang.org/x/exp/slices"
+	"strings"
+	"testing"
+)
 
 func TestParseEmptyFile(t *testing.T) {
 	fullText := ""
@@ -15,4 +19,33 @@ func TestParseEmptyFile(t *testing.T) {
 	if ast.nodeType != Program {
 		t.Errorf("Expected root node to have type Program, got %q", ast.nodeType)
 	}
+}
+
+func TestParseClassLeadingComments(t *testing.T) {
+	leadingComments := []string{`/*
+    * This is a leading comment.
+    */`,
+		`/* This is another leading comments. */`}
+	fullText := strings.Join(leadingComments, "") + `
+    class MyClass {
+        // Logic.
+    }`
+
+	ast := Parse(fullText)
+	classDeclarationNode := ast.children[0]
+
+	if classDeclarationNode.nodeType != ClassDeclaration {
+		t.Errorf("Expected a class node, got %q", classDeclarationNode.nodeType)
+	}
+
+	leadingCommentsFound := classDeclarationNode.leadingComments
+
+	if len(leadingCommentsFound) != 2 {
+		t.Errorf("Expected two leading comments, got %q", len(leadingCommentsFound))
+	}
+
+	if !slices.Equal(classDeclarationNode.leadingComments, leadingComments) {
+		t.Errorf("Didn't find the leading comments expected: %q != %q", classDeclarationNode.leadingComments, leadingComments)
+	}
+
 }
